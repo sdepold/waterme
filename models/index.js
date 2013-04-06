07b2646c1c93e6feea62e34cf47bcb9ffb6f96a0
Sascha Depold <sascha@depold.com>
2013-04-06 16:53:18 +0200

    fixed color of db

--- models/index.js
+++ models/index.js
@@ -4,9 +4,9 @@ if (!global.hasOwnProperty('db')) {
   var Sequelize = require('sequelize')
     , sequelize = null
 
-  if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+  if (process.env.HEROKU_POSTGRESQL_YELLOW_URL) {
     // the application is executed on Heroku ... use the postgres database
-    var match = process.env.HEROKU_POSTGRESQL_BRONZE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+    var match = process.env.HEROKU_POSTGRESQL_YELLOW_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
 
     sequelize = new Sequelize(match[5], match[1], match[2], {
       dialect:  'postgres',
